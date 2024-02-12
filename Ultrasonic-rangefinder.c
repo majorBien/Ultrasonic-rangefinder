1a70b3906e3734bb24683548ec91558db0e2563e
shadoww7 <122567282+shadoww7@users.noreply.github.com>
2024-02-12 13:09:07 +0100

    Update Ultrasonic-rangefinder.c

--- Ultrasonic-rangefinder.c
+++ Ultrasonic-rangefinder.c
@@ -56,7 +56,7 @@ void delay (uint16_t time)
 uint32_t IC_Val1 = 0;
 uint32_t IC_Val2 = 0;
 uint32_t Difference = 0;
-uint8_t Is_First_Captured = 0;  // is the first value captured ?
+uint8_t Is_First_Captured = 0;  
 uint8_t Distance  = 0;
  
 #define TRIG_PIN GPIO_PIN_4
@@ -64,20 +64,20 @@ uint8_t Distance  = 0;
  
 void HAL_TIM_IC_CaptureCallback(TIM_HandleTypeDef *htim)
 {
-    if (htim->Channel == HAL_TIM_ACTIVE_CHANNEL_1)  // if the interrupt source is channel1
+    if (htim->Channel == HAL_TIM_ACTIVE_CHANNEL_1)  
     {
-        if (Is_First_Captured==0) // if the first value is not captured
+        if (Is_First_Captured==0)
         {
-            IC_Val1 = HAL_TIM_ReadCapturedValue(htim, TIM_CHANNEL_1); // read the first value
-            Is_First_Captured = 1;  // set the first captured as true
-            // Now change the polarity to falling edge
+            IC_Val1 = HAL_TIM_ReadCapturedValue(htim, TIM_CHANNEL_1);
+            Is_First_Captured = 1;  
+           
             __HAL_TIM_SET_CAPTUREPOLARITY(htim, TIM_CHANNEL_1, TIM_INPUTCHANNELPOLARITY_FALLING);
         }
  
-        else if (Is_First_Captured==1)   // if the first is already captured
+        else if (Is_First_Captured==1)  
         {
-            IC_Val2 = HAL_TIM_ReadCapturedValue(htim, TIM_CHANNEL_1);  // read second value
-            __HAL_TIM_SET_COUNTER(htim, 0);  // reset the counter
+            IC_Val2 = HAL_TIM_ReadCapturedValue(htim, TIM_CHANNEL_1); 
+            __HAL_TIM_SET_COUNTER(htim, 0);  
  
             if (IC_Val2 > IC_Val1)
             {
@@ -90,9 +90,9 @@ void HAL_TIM_IC_CaptureCallback(TIM_HandleTypeDef *htim)
             }
  
             Distance = Difference * .034/2;
-            Is_First_Captured = 0; // set it back to false
+            Is_First_Captured = 0; 
  
-            // set polarity to rising edge
+          
             __HAL_TIM_SET_CAPTUREPOLARITY(htim, TIM_CHANNEL_1, TIM_INPUTCHANNELPOLARITY_RISING);
             __HAL_TIM_DISABLE_IT(&htim1, TIM_IT_CC1);
         }
@@ -102,9 +102,9 @@ void HAL_TIM_IC_CaptureCallback(TIM_HandleTypeDef *htim)
  
 void HCSR04_Read (void)
 {
-    HAL_GPIO_WritePin(TRIG_PORT, TRIG_PIN, GPIO_PIN_SET);  // pull the TRIG pin HIGH
-    delay(10);  // wait for 10 us
-    HAL_GPIO_WritePin(TRIG_PORT, TRIG_PIN, GPIO_PIN_RESET);  // pull the TRIG pin low
+    HAL_GPIO_WritePin(TRIG_PORT, TRIG_PIN, GPIO_PIN_SET);  
+    delay(10);  
+    HAL_GPIO_WritePin(TRIG_PORT, TRIG_PIN, GPIO_PIN_RESET);  
  
     __HAL_TIM_ENABLE_IT(&htim1, TIM_IT_CC1);
 }
